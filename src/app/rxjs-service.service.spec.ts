31d6e4a2ed896d76670a0ac25af1cbe605231c30
ilya.lobov <ilya.lobov@indusoft.ru>
2020-12-11 17:49:43 +0300

    as Propery Works
    multicast Works

--- src/app/rxjs-service.service.spec.ts
+++ src/app/rxjs-service.service.spec.ts
@@ -33,30 +33,39 @@ describe('RxjsServiceService', () => {
     const expected =     cold('^------!');
     const source = service.subscribeToShipmentDayFirst(main, first, second);
     */
-
     const values =    { a: 1 , b: 2, c: 3};
     // const obs1 =     cold('-a-------a--', values);
     // const obs2 =     cold('-a-------a--', values);
     // const obs3 =     cold('-a-------a--', values);
     // const expected = cold('-a----------', values);
-
-
-    const obs1 =     cold('a-a', values);
-    const obs2 =     cold('bac', values);
-    const expected = cold('-bac-bac', values );
-
+    const obs1 =     cold('-a-', {a:1});
+    const obs2 =     cold('-a-', {a:1});
+    //const expected = cold('-bac-bac', values );
+    const expected = hot('--a', {a:1});
     const result =  service.subscribeToShipmentDayFirst(obs1, obs2);
     result.pipe(tap(r => {
       console.log('asdasdada', r)
     }));
-    expect(result).toBeObservable(expected);
-
-
+    result.subscribe(r=>{
+                    expect(r).toBeObservable(expected);
+      })
     /*
-
-
-
     expect(source).toBeObservable(expected);
     expect(source).toHaveSubscriptions(subscription);*/
+  });
+  
+  it('should ', () => {
+    const things   = 'a-b-a';
+    const expected =  't-f-t';
+    // spy on
+    spyOn(service['things'], 'asObservable').and.returnValue(hot(things, {
+      a: ['a', 'b'],
+      b: []
+    }));
+
+    expect(service.things$).toBeObservable(cold(expected, {
+      t: true,
+      f: false
+    }));
   });
 });
